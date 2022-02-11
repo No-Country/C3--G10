7db60d9936ef9513cb3b01733bb6dcfa8d8c20e1
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2022-02-11 14:56:36 -0300

    Project created

---
+++ src/test/java/com/nocountry/grupo10/Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.grupo10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
