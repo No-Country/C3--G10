028f7a4b640990b1f195f44085e9e3fb9400c2d9
carop2580 <perezcarolinasn@gmail.com>
2022-02-16 11:45:18 -0300

    modificar Account

--- src/main/java/com/nocountry/grupo10/model/entity/Account.java
+++ src/main/java/com/nocountry/grupo10/model/entity/Account.java
@@ -4,11 +4,18 @@ package com.nocountry.grupo10.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
-@Table (name= "Account")
+@Table (name= "account")
 @Getter
 @Setter
 public class Account implements Serializable {
